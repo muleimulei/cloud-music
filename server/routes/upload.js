cde47f68d5d9064fd87c3b1c14b81fa0cfb7e685
muleimulei <18729274217@163.com>
2017-08-06 23:51:27 +0800

    upload

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var upload = require('multer')({dest: '../public/media'});
 
-router.post('/music', function (req, res, next) {
+router.post('/music', upload.single('file') ,function (req, res, next) {
+    // 没有附带文件
+     if (!req.file) {
+        res.json({ ok: false });
+        return;
+     }
+
+     // 输出文件信息
+     console.log('====================================================');
+     console.log('fieldname: ' + req.file.fieldname);
+     console.log('originalname: ' + req.file.originalname);
+     console.log('encoding: ' + req.file.encoding);
+     console.log('mimetype: ' + req.file.mimetype);
+     console.log('size: ' + (req.file.size / 1024).toFixed(2) + 'KB');
+     console.log('destination: ' + req.file.destination);
+     console.log('filename: ' + req.file.filename);
+     console.log('path: ' + req.file.path);
 
 });
 
